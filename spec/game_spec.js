f87e9f86b7a9cbf645d33cea3d5c6dbd2f577d0e
Cody Jenkins <iknowcss@gmail.com>
2013-03-06 13:31:03 -0800

    Initial spec and code for game object

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -1,4 +1,5 @@
-var game = require('../lib/game'),
+var game = require('../lib/game')
+    _ = require('underscore'),
     
     gameStatus = game.gameStatus;
 
@@ -9,13 +10,33 @@ describe('A Game', function () {
         gameInstance = game();
     });
 
-    it('is constructed in the "ready" state', function () {
-        expect(gameInstance.status()).toBe('ready');
+    it('is constructed in the "ready" state by default', function () {
+        expect(gameInstance.status()).toBe(gameStatus.READY);
     });
 
-    it('should change its status to "in-progress" when started', function () {
-        gameInstance.startGame();
-        expect(gameInstance.status()).toBe('in-progress');
+    it('should change its status appropriately from game start to game end', function () {
+        expect(gameInstance.startGame()).toBeTruthy();
+        expect(gameInstance.status()).toBe(gameStatus.IN_PROGRESS);
+        expect(gameInstance.endGame()).toBeTruthy();
+        expect(gameInstance.status()).toBe(gameStatus.ENDED);
+    });
+
+    it('should change its status appropriately from game start to game cancelled', function () {
+        expect(gameInstance.startGame()).toBeTruthy();
+        expect(gameInstance.status()).toBe(gameStatus.IN_PROGRESS);
+        expect(gameInstance.cancelGame()).toBeTruthy();
+        expect(gameInstance.status()).toBe(gameStatus.CANCELLED);
+    });
+
+    it('should not start a game when "in-progress", "cancelled", or "ended"', function () {
+        gameInstance = game({ status: gameStatus.IN_PROGRESS });
+        expect(gameInstance.startGame()).toBeFalsy();
+        
+        gameInstance = game({ status: gameStatus.CANCELLED });
+        expect(gameInstance.startGame()).toBeFalsy();
+        
+        gameInstance = game({ status: gameStatus.ENDED });
+        expect(gameInstance.startGame()).toBeFalsy();
     });
 
 });
