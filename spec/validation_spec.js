d8578b6f954a6b7a9fbc9d8698df5d4b3140af6c
Cody Jenkins <cody.jenkins@resmed.com>
2013-03-07 18:07:04 -0800

    Added stub for validation module

---
+++ spec/validation_spec.js
@@ -0,0 +1,16 @@
+var Validation = require('../lib/validation'),
+	_ = require('underscore');
+
+describe('A Validation', function () {
+
+	var validation;
+
+	beforeEach(function () {
+		validation = Validation();
+	});
+
+	it('should init', function () {
+		
+	});
+
+});
