ce670634f36fb751c1c95e538b145335601b0da8
Cody Jenkins <iknowcss@gmail.com>
2013-03-11 09:03:17 -0700

    Created new game object and started fleshing out more of point-counter

--- spec/game-state_spec.js
+++ spec/game-state_spec.js
@@ -1,5 +1,4 @@
 var GameState = require('../lib/game-state'),
-    PlayerSet = require('../lib/player-set'),
     _ = require('underscore');
 
 describe('A GameState', function () {
@@ -11,74 +10,34 @@ describe('A GameState', function () {
 
     it('should change its status appropriately from game start to game end', function () {
         game.startGame();
-        expect(game.getStatus()).toBe(GameState.status.IN_PROGRESS);
+        expect(game.getStatus()).toBe(GameState.IN_PROGRESS);
 
         game.endGame();
-        expect(game.getStatus()).toBe(GameState.status.ENDED);
+        expect(game.getStatus()).toBe(GameState.ENDED);
     });
 
     it('should change its status appropriately from game start to game cancelled', function () {
         game.startGame();
-        expect(game.getStatus()).toBe(GameState.status.IN_PROGRESS);
+        expect(game.getStatus()).toBe(GameState.IN_PROGRESS);
         
         game.cancelGame();
-        expect(game.getStatus()).toBe(GameState.status.CANCELLED);
+        expect(game.getStatus()).toBe(GameState.CANCELLED);
     });
 
     it('should not start a game when "in-progress", "cancelled", or "ended"', function () {
-        expectNoStatusChange(GameState.status.IN_PROGRESS, "startGame");
-        expectNoStatusChange(GameState.status.CANCELLED, "startGame");
-        expectNoStatusChange(GameState.status.ENDED, "startGame");
+        expectNoStatusChange(GameState.IN_PROGRESS, "startGame");
+        expectNoStatusChange(GameState.CANCELLED, "startGame");
+        expectNoStatusChange(GameState.ENDED, "startGame");
     });
 
     it('should not cancel or end a game when "ready", "cancelled", or "ended"', function () {
-        expectNoStatusChange(GameState.status.READY, "cancelGame");
-        expectNoStatusChange(GameState.status.CANCELLED, "cancelGame");
-        expectNoStatusChange(GameState.status.ENDED, "cancelGame");
+        expectNoStatusChange(GameState.READY, "cancelGame");
+        expectNoStatusChange(GameState.CANCELLED, "cancelGame");
+        expectNoStatusChange(GameState.ENDED, "cancelGame");
 
-        expectNoStatusChange(GameState.status.READY, "endGame");
-        expectNoStatusChange(GameState.status.CANCELLED, "endGame");
-        expectNoStatusChange(GameState.status.ENDED, "endGame");
-    });
-
-    it('should not validate when an invalid playerSet is set', function () {
-        var validation,
-            nonPlayerSet = {},
-            incompletePlayerSet = new PlayerSet('Lone player'),
-            invalidPlayerSet = new PlayerSet(4.3, 5),
-            validPlayerSet = new PlayerSet('Player X', 'Player Y');
-
-        game = new GameState({ playerSet: nonPlayerSet });
-        validation = game.validate();
-        expect(validation.valid).toBe(false);
-        expect(validation.errors.length).toBe(1);
-
-        game = new GameState({ playerSet: incompletePlayerSet });
-        validation = game.validate();
-        expect(validation.valid).toBe(false);
-        expect(validation.errors.length).toBe(1);
-
-        game = new GameState({ playerSet: invalidPlayerSet });
-        validation = game.validate();
-        expect(validation.valid).toBe(false);
-        expect(validation.errors.length).toBe(2);
-        
-        game = new GameState({ playerSet: validPlayerSet });
-        validation = game.validate();
-        expect(validation.valid).toBe(true);
-        expect(validation.errors.length).toBe(0);
-
-    });
-
-    it('should generate an up-to-date JSON', function () {
-        var expectedJson = {
-                status: GameState.status.IN_PROGRESS,
-                playerSet: {
-                    players: ['Player 1', 'Player 2']
-                }
-            };
-        game.startGame();
-        expect(game.toJSON()).toEqual(expectedJson);
+        expectNoStatusChange(GameState.READY, "endGame");
+        expectNoStatusChange(GameState.CANCELLED, "endGame");
+        expectNoStatusChange(GameState.ENDED, "endGame");
     });
 
     function expectNoStatusChange (initialStatus, action) {
--- spec/point-counter_spec.js
+++ spec/point-counter_spec.js
@@ -10,10 +10,54 @@ describe('A PointCounter', function () {
         pointCounter = new PointCounter();
     });
 
-    it('should return an independent, cloned points array', function () {
-        var points = pointCounter.getPoints();
-        points[0] = 9001;
-        expect(pointCounter.getPoints()).not.toEqual(points);
+    it('should return an independent, cloned score array', function () {
+        var score = pointCounter.getScore();
+        score[0] = 9001;
+        expect(pointCounter.getScore()).not.toEqual(score);
+    });
+
+    it('should not validate with invalid initial score', function () {
+        var wrongType = {},
+            wrongSize = [1],
+            wrongElementType = [1.2, 1.4],
+            invalidElementValue = [-1, -2],
+            validPoints = [0, 5],
+            validation;
+
+        pointCounter = new PointCounter({ initialScore: wrongType });
+        validation = pointCounter.validate();
+        expect(validation.valid).toBe(false);
+        expect(validation.errors.length).toBe(1);
+
+        pointCounter = new PointCounter({ initialScore: wrongSize });
+        validation = pointCounter.validate();
+        expect(validation.valid).toBe(false);
+        expect(validation.errors.length).toBe(1);
+
+        pointCounter = new PointCounter({ initialScore: wrongElementType });
+        validation = pointCounter.validate();
+        expect(validation.valid).toBe(false);
+        expect(validation.errors.length).toBe(2);
+
+        pointCounter = new PointCounter({ initialScore: invalidElementValue });
+        validation = pointCounter.validate();
+        expect(validation.valid).toBe(false);
+        expect(validation.errors.length).toBe(2);
+
+        pointCounter = new PointCounter({ initialScore: validPoints });
+        validation = pointCounter.validate();
+        expect(validation.valid).toBe(true);
+        expect(validation.errors.length).toBe(0);
+    });
+
+    it('should allow points to be added to the "left" or "right"', function () {
+        pointCounter.pointLeft();
+        pointCounter.pointRight();
+        pointCounter.pointLeft();
+        pointCounter.pointLeft();
+        pointCounter.pointRight();
+
+        expect(pointCounter.getScore()).toEqual([3, 2]);
     });
 
 });
