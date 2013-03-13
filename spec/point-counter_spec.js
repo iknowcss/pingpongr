2133a1811052cefd00a945aeb918d7bdb5f15713
Cody Jenkins <cody.jenkins@resmed.com>
2013-03-13 12:17:58 -0700

    Finished making point-counter initialize in jQuery fashion and updated specs to test

--- spec/point-counter_spec.js
+++ spec/point-counter_spec.js
@@ -4,144 +4,165 @@ var PointCounter = require('../lib/point-counter'),
 
 describe('A PointCounter', function () {
 
-    var pointCounter = PointCounter();
+    var globalPointCounter;
+
+    function doConstructWith (initArg) {
+        return function () {
+            if (!_.isUndefined(initArg)) {
+                globalPointCounter = PointCounter(initArg);
+            } else {
+                globalPointCounter = PointCounter();
+            }
+        };
+    }
 
     it('should initialize with defaults', function () {
-        var construct = function () {
-            pointCounter = PointCounter();
-        }
-        expect(pointCounter instanceof PointCounter).toBe(true);
-        expect(construct).not.toThrow();
-        expect(pointCounter.getScore()).toEqual([0, 0]);
+        expect(doConstructWith()).not.toThrow();
+        expect(globalPointCounter instanceof PointCounter).toBe(true);
+        expect(globalPointCounter.getScore()).toEqual([0, 0]);
     });
 
-    // it('should return an independent, cloned score array', function () {
-    //     var score = pointCounter.getScore();
-    //     score[0] = 9001;
-    //     expect(pointCounter.getScore()).not.toEqual(score);
-    // });
+    it('should initialize with a score array as the constructor argument', function () {
+        var validScore = [5, 1];
 
-    // it('should not validate with invalid initial score', function () {
-    //     var wrongType = 1,
-    //         wrongSize = [1],
-    //         wrongElementType = [1.2, 1.4],
-    //         invalidElementValue = [-1, -2],
-    //         validPoints = [0, 5],
-    //         validation;
+        expect(doConstructWith(validScore)).not.toThrow();
+        expect(globalPointCounter instanceof PointCounter).toBe(true);
+        expect(globalPointCounter.getScore()).toEqual([5, 1]);
+    });
 
-    //     pointCounter = new PointCounter(wrongType);
-    //     validation = pointCounter.validate();
-    //     expect(validation.valid).toBe(false);
-    //     expect(validation.errors.length).toBe(1);
+    it('should initialize an independent instance with a PointCounter as the constructor argument', function () {
+        var initialScore = [1, 5],
+            otherPointCounter = PointCounter(initialScore);
 
-    //     pointCounter = new PointCounter({ initialScore: wrongType });
-    //     validation = pointCounter.validate();
-    //     expect(validation.valid).toBe(false);
-    //     expect(validation.errors.length).toBe(1);
+        expect(doConstructWith(otherPointCounter)).not.toThrow();
+        expect(globalPointCounter instanceof PointCounter).toBe(true);
+        expect(globalPointCounter.getScore()).toEqual(initialScore);
 
-    //     pointCounter = new PointCounter({ initialScore: wrongSize });
-    //     validation = pointCounter.validate();
-    //     expect(validation.valid).toBe(false);
-    //     expect(validation.errors.length).toBe(1);
+        otherPointCounter.pointLeft();
+        expect(otherPointCounter.getScore()).toEqual([2, 5]);
+        expect(globalPointCounter.getScore()).toEqual(initialScore);
+    });
 
-    //     pointCounter = new PointCounter({ initialScore: wrongElementType });
-    //     validation = pointCounter.validate();
-    //     expect(validation.valid).toBe(false);
-    //     expect(validation.errors.length).toBe(2);
+    it('should not initialize with a bad constructor argument', function () {
+        var wrongType = 1,
+            wrongSize = [1],
+            wrongElementType = ["0", "0"],
+            invalidButNotBad = [-1, 0];
 
-    //     pointCounter = new PointCounter({ initialScore: invalidElementValue });
-    //     validation = pointCounter.validate();
-    //     expect(validation.valid).toBe(false);
-    //     expect(validation.errors.length).toBe(2);
+        expect(doConstructWith(wrongType)).toThrow();
+        expect(doConstructWith(wrongSize)).toThrow();
+        expect(doConstructWith(wrongElementType)).toThrow();
+        expect(doConstructWith(invalidButNotBad)).not.toThrow();
+    });
 
-    //     pointCounter = new PointCounter({ initialScore: validPoints });
-    //     validation = pointCounter.validate();
-    //     expect(validation.valid).toBe(true);
-    //     expect(validation.errors.length).toBe(0);
-    // });
+    it('should not validate with an invalid score', function () {
+        var wrongNumberType = [5.5, 0.2],
+            negativeNumber = [-1, -5],
+            validation;
 
-    // it('should allow points to be added to the "left" or "right"', function () {
-    //     var score;
+        validation = PointCounter(wrongNumberType).validate();
+        expect(validation.valid).toBe(false);
+        expect(validation.errors.length).toBe(2);
 
-    //     score = pointCounter.pointLeft();
-    //     expect(score).toEqual([1, 0]);
-    //     score = pointCounter.pointRight();
-    //     expect(score).toEqual([1, 1]);
-    //     score = pointCounter.pointLeft();
-    //     expect(score).toEqual([2, 1]);
-    //     score = pointCounter.pointLeft();
-    //     expect(score).toEqual([3, 1]);
-    //     score = pointCounter.pointRight();
-    //     expect(score).toEqual([3, 2]);
+        validation = PointCounter(negativeNumber).validate();
+        expect(validation.valid).toBe(false);
+        expect(validation.errors.length).toBe(2);
+    });
 
-    //     expect(pointCounter.getScore()).toEqual([3, 2]);
-    // });
+    it('should return an independent, cloned score array', function () {
+        var pointCounter = PointCounter(),
+            score = pointCounter.getScore();
+        score[0] = 9001;
+        expect(pointCounter.getScore()).not.toEqual(score);
+    });
 
-    // it('should allow points to be undone and redone', function () {
-    //     var score;
+    it('should allow points to be added to the "left" or "right"', function () {
+        var pointCounter = PointCounter(),
+            score;
 
-    //     // Undo points
-    //     pointCounter.pointLeft();          // 1 to 0
-    //     pointCounter.pointRight();         // 1 to 1
-    //     score = pointCounter.undoPoint();  // 1 to 0
-    //     expect(score).toEqual([1, 0]);
-    //     score = pointCounter.undoPoint();  // 0 to 0
-    //     expect(score).toEqual([0, 0]);
-    //     score = pointCounter.undoPoint();  // 0 to 0 (still)
-    //     expect(score).toEqual([0, 0]);
+        score = pointCounter.pointLeft();
+        expect(score).toEqual([1, 0]);
+        score = pointCounter.pointRight();
+        expect(score).toEqual([1, 1]);
+        score = pointCounter.pointLeft();
+        expect(score).toEqual([2, 1]);
+        score = pointCounter.pointLeft();
+        expect(score).toEqual([3, 1]);
+        score = pointCounter.pointRight();
+        expect(score).toEqual([3, 2]);
 
-    //     // Continue after undoing "too far"
-    //     score = pointCounter.pointRight(); // 0 to 1
-    //     expect(score).toEqual([0, 1]);
+        expect(pointCounter.getScore()).toEqual([3, 2]);
+    });
 
-    //     // Prepare a state and then undo thrice
-    //     pointCounter.pointRight(); // 0 to 2
-    //     pointCounter.pointLeft();  // 1 to 2
-    //     pointCounter.pointLeft();  // 2 to 2
-    //     pointCounter.pointRight(); // 2 to 3 *
-    //     pointCounter.undoPoint();  // 2 to 2
-    //     pointCounter.undoPoint();  // 1 to 2
-    //     pointCounter.undoPoint();  // 0 to 2
+    it('should allow points to be undone', function () {
+        var pointCounter = PointCounter(),
+            score;
 
-    //     // Test redo
-    //     score = pointCounter.redoPoint(); // 1 to 2
-    //     expect(score).toEqual([1, 2]);
-    //     score = pointCounter.redoPoint(); // 2 to 2
-    //     expect(score).toEqual([2, 2]);
+        // Undo before even started
+        score = pointCounter.undoPoint();  // 0 to 0
+        expect(score).toEqual([0, 0]);
 
-    //     // Overwrite the 3rd point
-    //     score = pointCounter.pointLeft(); // 3 to 2
-    //     expect(score).toEqual([3, 2]);
+        // Undo points
+        pointCounter.pointLeft();          // 1 to 0
+        pointCounter.pointRight();         // 1 to 1
+        score = pointCounter.undoPoint();  // 1 to 0
+        expect(score).toEqual([1, 0]);
+        score = pointCounter.undoPoint();  // 0 to 0
+        expect(score).toEqual([0, 0]);
+        score = pointCounter.undoPoint();  // 0 to 0 (still)
+        expect(score).toEqual([0, 0]);
 
-    //     // Redo past current score
-    //     score = pointCounter.redoPoint(); // 3 to 2 (still)
-    //     expect(score).toEqual([3, 2]);
-    // });
+        // Continue after undoing "too far"
+        score = pointCounter.pointRight(); // 0 to 1
+        expect(score).toEqual([0, 1]);
+    });
 
-    // it('should generate an up-to-date JSON', function () {
-    //     var initialPointsJSON = {
-    //             score: [0, 0]
-    //         },
-    //         leftPointsJSON = {
-    //             score: [1, 0]
-    //         },
-    //         bothPointsJSON = {
-    //             score: [1, 1]
-    //         },
-    //         undoneJSON = {
-    //             score: [1, 0]
-    //         };
+    it('should allow points to be redone after being undone', function () {
+        var pointCounter = PointCounter(),
+            score;
 
-    //     expect(pointCounter.toJSON()).toEqual(initialPointsJSON);
+        // Prepare a state and then undo thrice
+        pointCounter.pointRight(); // 0 to 1
+        pointCounter.pointRight(); // 0 to 2
+        pointCounter.pointLeft();  // 1 to 2
+        pointCounter.pointLeft();  // 2 to 2
+        pointCounter.pointRight(); // 2 to 3 *
+        pointCounter.undoPoint();  // 2 to 2
+        pointCounter.undoPoint();  // 1 to 2
+        pointCounter.undoPoint();  // 0 to 2
 
-    //     pointCounter.pointLeft();
-    //     expect(pointCounter.toJSON()).toEqual(leftPointsJSON);
+        // Test redo
+        score = pointCounter.redoPoint(); // 1 to 2
+        expect(score).toEqual([1, 2]);
+        score = pointCounter.redoPoint(); // 2 to 2
+        expect(score).toEqual([2, 2]);
 
-    //     pointCounter.pointRight();
-    //     expect(pointCounter.toJSON()).toEqual(bothPointsJSON);
+        // Overwrite the 3rd point
+        score = pointCounter.pointLeft(); // 3 to 2
+        expect(score).toEqual([3, 2]);
 
-    //     pointCounter.undoPoint();
-    //     expect(pointCounter.toJSON()).toEqual(undoneJSON);
-    // });
+        // Redo beyond current score
+        score = pointCounter.redoPoint(); // 3 to 2 (still)
+        expect(score).toEqual([3, 2]);
+    });
+
+    it('should generate an up-to-date JSON', function () {
+        var pointCounter = PointCounter(),
+            initialPointsJSON = [0, 0],
+            leftPointsJSON = [1, 0],
+            bothPointsJSON = [1, 1],
+            undoneJSON = [1, 0];
+
+        expect(pointCounter.toJSON()).toEqual(initialPointsJSON);
+
+        pointCounter.pointLeft();
+        expect(pointCounter.toJSON()).toEqual(leftPointsJSON);
+
+        pointCounter.pointRight();
+        expect(pointCounter.toJSON()).toEqual(bothPointsJSON);
+
+        pointCounter.undoPoint();
+        expect(pointCounter.toJSON()).toEqual(undoneJSON);
+    });
 
 });
