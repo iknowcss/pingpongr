0e9a81fac33d451dbac9c19895115d421d3f34f5
Cody Jenkins <iknowcss@gmail.com>
2013-03-24 18:30:49 -0700

    Continued work on validation-error module

--- spec/validation/validation-error_spec.js
+++ spec/validation/validation-error_spec.js
@@ -11,11 +11,53 @@ describe('A ValidationError', function () {
         };
     }
 
-    it('may be constructed with a single message', function () {
+    it('may be constructed with a single string message', function () {
         var error = 'Game not found';
 
         expect(doConstruct(error)).not.toThrow();
         expect(globalValidationError instanceof ValidationError).toBe(true);
-    })
+        expect(globalValidationError.getErrors()).toEqual([error]);
+    });
+
+    it('may be constructed with an array of string messages', function () {
+        var errors = [
+                'Player 1 had an invalid score',
+                'Player 2 had an invalid score',
+            ];
+
+        expect(doConstruct(errors)).not.toThrow();
+        expect(globalValidationError instanceof ValidationError).toBe(true);
+        expect(globalValidationError.getErrors()).toEqual(errors);
+    });
+
+    it('throws an exception when a bad construction argument is provided', function () {
+        var badType = {}
+          , emptyArray = []
+          , badArrayElement = ['Normal error', {}];
+
+        expect(doConstruct()).toThrow();
+        expect(doConstruct(badType)).toThrow();
+        expect(doConstruct(emptyArray)).toThrow();
+        expect(doConstruct(badArrayElement)).toThrow();
+    });
+
+    it('constructs with a cloned, independent errors array', function () {
+        var errors = ['one', 'two']
+          , expectedErrors = _.clone(errors)
+          , validationError = ValidationError(errors);
+
+        errors[0] = 'three';
+        expect(validationError.getErrors()).toEqual(expectedErrors);
+    });
+
+    it('returns a cloned, independent errors array', function () {
+        var returnedErrors
+          , initErrors = ['one', 'two']
+          , validationError = ValidationError(initErrors);
+
+        returnedErrors = validationError.getErrors();
+        returnedErrors[0] = 'three';
+        expect(validationError.getErrors()).toEqual(initErrors);
+    });
 
 });
